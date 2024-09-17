dd6ee9e293afdc37bf6bcf35fd083605f3d7809a
Kulimák Máté <kulimakmate@turr.hu>
2024-09-17 11:35:49 +0200

    Minden amit az első órán haladtam

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -3,6 +3,7 @@ const Appversion = "v1.0";
 const Author = "Kulimák Máté és Tátrai Dominik Oszkár";
 const Company = "Bajai SZC Türr István Technikum";
 
+
 const serverUrl = 'http://localhost:3000';
 
 let title = document.querySelector('title');
@@ -12,3 +13,29 @@ let footer = document.querySelector('footer');
 title.innerHTML = AppTitle + ' ' + Appversion;
 header.innerHTML = AppTitle;
 footer.innerHTML = Company + ' | ' + Author + ' | 2024.';
+
+async function render(view){
+    let main = document.querySelector('main');
+    main.innerHTML = await (await fetch(`Views/${view}.html`)).text();
+    header.innerHTML = String(view);
+
+    switch(view){
+        case 'profile': {
+            getMe();
+            break;
+        }
+        case 'users': {
+            getUsers();
+            break;
+        }
+        case 'steps': {
+            getStepDatas();
+            break;
+        }
+        case 'statistics': {
+            getUserStats();
+            getAdminStats(); // feltételhez kell kötni! hogy admin-e a bejelentkezett user
+            break;
+        }
+    }
+}
