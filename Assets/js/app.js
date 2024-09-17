ba2180a34fd934b1e7f832074b9026978b9a4f7d
TatraiDominik <tatraidominikoszkar@turr.hu>
2024-09-17 12:27:45 +0200

    Főoldal design

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -2,23 +2,23 @@ const AppTitle = "Online Szakácskönyv";
 const Appversion = "v1.0";
 const Author = "Kulimák Máté és Tátrai Dominik Oszkár";
 const Company = "Bajai SZC Türr István Technikum";
-
-
+ 
+ 
 const serverUrl = 'http://localhost:3000';
-
+ 
 let title = document.querySelector('title');
 let header = document.querySelector('header');
 let footer = document.querySelector('footer');
-
+ 
 title.innerHTML = AppTitle + ' ' + Appversion;
 header.innerHTML;
 footer.innerHTML = Company + ' | ' + Author + ' | 2024.';
-
+ 
 async function render(view){
     let main = document.querySelector('main');
     main.innerHTML = await (await fetch(`Views/${view}.html`)).text();
     header.innerHTML = String(view);
-
+ 
     switch(view){
         case 'profile': {
             getMe();
@@ -39,3 +39,9 @@ async function render(view){
         }
     }
 }
+ 
+document.addEventListener('DOMContentLoaded', function() {
+    if (typeof render === 'function') {
+        render('Főoldal');
+    }
+});
