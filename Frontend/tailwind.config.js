8931c4e65c85a257e1ef1f29dcef940ebde6bee3
TatraiDominik <tatraidominikoszkar@turr.hu>
2024-09-18 12:18:11 +0200

    tailwind setup

---
+++ Frontend/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./{Assets,Views}/**/*.{html,js}", "./index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
