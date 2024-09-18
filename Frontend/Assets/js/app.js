035c6dbbd49bdb99353000448bce75acc7567d3a
Kulimák Máté <kulimakmate@turr.hu>
2024-09-18 11:49:03 +0200

    A projektunk uj alapkove

--- Frontend/Assets/js/app.js
+++ Frontend/Assets/js/app.js
@@ -2,29 +2,23 @@ const AppTitle = "Online Szakácskönyv";
 const Appversion = "v1.0";
 const Author = "Kulimák Máté és Tátrai Dominik Oszkár";
 const Company = "Bajai SZC Türr István Technikum";
- 
- 
+
+loggedUser = null;
+
 const serverUrl = 'http://localhost:3000';
- 
+
 let title = document.querySelector('title');
 let header = document.querySelector('header');
-//let footer = document.querySelector('footer');
- 
+let footer = document.querySelector('footer');
+
 title.innerHTML = AppTitle + ' ' + Appversion;
 header.innerHTML;
-//footer.innerHTML = Company + ' | ' + Author + ' | 2024.';
- 
+footer.innerHTML = Company + ' | ' + Author + ' | 2024.';
+
 async function render(view){
     let main = document.querySelector('main');
     main.innerHTML = await (await fetch(`Views/${view}.html`)).text();
-    if(view == 'Főoldal'){
-
-        header.classList.add('remove')
-    }
-    else{
-        header.innerHTML = String(view);
-        header.classList.remove('remove')
-    }
+    header.innerHTML = String(view);
 
     switch(view){
         case 'profile': {
@@ -46,10 +40,51 @@ async function render(view){
         }
     }
 }
+
+if (localStorage.getItem('szakacskonyv')){
+    loggedUser = JSON.parse(localStorage.getItem('szakacskonyv'));
+    render('Főoldal');
+}else{
+    render('Bejelentkezés');
+}
+
+function renderNavItems(){
+    let lgdOutNavItems = document.querySelectorAll('.lgdOut');
+    let lgdInNavItems = document.querySelectorAll('.lgdIn');
+    let admNavItems = document.querySelectorAll('.lgdAdm');
+
+    // ha nem vagyunk bejelentkezve
+    if (loggedUser == null){
+        lgdInNavItems.forEach(item =>{
+            item.classList.add('d-none');
+        });
+        lgdOutNavItems.forEach(item => {
+            item.classList.remove('d-none');
+        });
+        admNavItems.forEach(item => {
+            item.classList.add('d-none');
+        });
+        return;
+    }
+    // admin vagyunk
+    if (loggedUser.role == 'admin'){
+        admNavItems.forEach(item => {
+            item.classList.remove('d-none');
+        });
+    }
  
+    // user vagyunk
+    lgdInNavItems.forEach(item => {
+        item.classList.remove('d-none');
+    });
+
+    lgdOutNavItems.forEach(item => {
+        item.classList.add('d-none');
+    });
+}
+
 document.addEventListener('DOMContentLoaded', function() {
     if (typeof render === 'function') {
         render('Főoldal');
-
     }
 });
--- Frontend/Controllers/userCtrl.js
+++ Frontend/Controllers/userCtrl.js
@@ -1,13 +1,41 @@
-
 function registration(){
+    console.log('Belépett');
     let newUser = {
         name: document.querySelector('#name').value,
         email: document.querySelector('#email').value,
         passwd: document.querySelector('#passwd').value,
-        confirm: document.querySelector('#confirm').value
+        confirm: document.querySelector('#confirm').value,
+        telefon: document.querySelector('#telefon').value
     }
 
     axios.post(`${serverUrl}/reg`, newUser).then(res => {
         alert(res.data);
     });
+}
+
+function login(){
+    let user = {
+        email: document.querySelector('#email').value,
+        password: document.querySelector('#passwd').value
+    }
+    console.log(user);
+
+    axios.post(`${serverUrl}/login`, user).then(res =>{
+        console.log("Ide belepett!");
+        if (res.status != 200){
+            alert(res.data);
+            return;
+        }
+
+        loggedUser = res.data;
+        localStorage.setItem('szakacskonyv', JSON.stringify(loggedUser));
+        render('Főoldal');
+    });
+}
+
+function logout(){
+    localStorage.removeItem('szakacskonyv');
+    loggedUser = null;
+    renderNavItems();
+    render('Főoldal');
 }
